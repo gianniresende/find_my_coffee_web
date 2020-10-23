a41bf23e3cb86c6fac5c5e805323c4ddd7237b6b
gianniresende <gianniresende@gmail.com>
2020-10-23 09:26:52 -0300

    refactor: add maps

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState, useEffect} from 'react';
 import { GoogleMap, LoadScript } from '@react-google-maps/api';
-​
 const App = () => {
+  const [latitude, setLatitude] = useState(0);
+  const [longitude, setLongitude] = useState(0);
+  const [locations, setLocations] = useState([]);
+  const [selected, setSelected] = useState({});
 
   const { REACT_GOOGLE_API_KEY } = process.env;
 
+  useEffect(() => {
+    setCurrentLocation();
+  }, []);
+  async function setCurrentLocation() {
+    await navigator.geolocation.getCurrentPosition(function (position) {
+      setLatitude(position.coords.latitude);
+      setLongitude(position.coords.longitude);
+      loadCoffeShops();
+      }, function(error) {
+        alert('Habilite a localização para utilizar o aplicativo!');
+    });
+  }
+
+  async function loadCoffeeShops() {
+    const response = await EstablishmentsService.index(latitude, longitude);
+    setLocations(response.data.results);
+  }
+
   return (
-    <Fragment>
-  ​     	
-        <LoadScript googleMapsApiKey={REACT_APP_GOOGLE_API_KEY}>
+    <Fragment>  ​     	
+        <LoadScript googleMapsApiKey={REACT_GOOGLE_API_KEY}>
           <GoogleMap mapContainerStyle={{height: "100vh", width: "100%"}}
             zoom={15}
-            center={{lat: -21.7768606, lng: -41.3109657}}>
+            center={{lat: -25.447306, lng: -49.269027}}>
           </GoogleMap>
         </LoadScript>
     </Fragment>
   );
 }
-​
 export default App;
